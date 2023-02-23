1927cf0a4645957ae3273173c3f5d92f5ea532cb
Matthieu Mary <matthieu.mary2@gmail.com>
2023-02-23 20:03:07 +0100

    add curve on session Time graph

--- SportSee-Front/src/components/SessionTime.tsx
+++ SportSee-Front/src/components/SessionTime.tsx
@@ -2,23 +2,22 @@ import USER_AVERAGE_SESSION from "../mocks/SessionTimeMock.js";
 import * as d3 from "d3";
 import { useState, useRef, useEffect } from "react";
 
-interface UserSession {
-  day: number;
-  sessionLength: number;
-}
+// interface UserSession {
+//   day: number;
+//   sessionLength: number;
+// }
 
 export default function SessionTime() {
   const [data] = useState(USER_AVERAGE_SESSION[0]);
-  const sessions: UserSession[] = data.sessions;
+  const sessions: any = data.sessions;
   const sessionLengthArr: number[] = data.sessions.map(
     (session) => session.sessionLength
   );
   const sessionMax: number = Math.max(...sessionLengthArr);
   const sessionMin: number = Math.min(...sessionLengthArr);
 
-  console.log(sessions);
-
-  const lineChart: any = useRef();
+  const lineChart: any = useRef(null);
+  const infosLineChart: any = useRef(null);
 
   useEffect(() => {
     lineChart.current.innerHTML = "";
@@ -37,23 +36,25 @@ export default function SessionTime() {
     const lineCurves = d3
       .line()
       .x((d, i) => (wChartWrapper / (sessions.length - 1)) * i)
-      .y((d, i) => (i * 100) / d.length)
+      .y((d: any, i) => d.sessionLength + 160)
       .curve(d3.curveNatural);
 
     // Create graph with curve
-    // lineChartWrapper
-    //   .append("path")
-    //   .attr("d", lineCurves(sessions))
-    //   .attr("stroke", "url(#linGrad)")
-    //   .attr("stroke-width", 2)
-    //   .attr("fill", "none");
+    lineChartWrapper
+      .append("path")
+      .attr("d", lineCurves(sessions))
+      .attr("stroke", "white")
+      .attr("stroke-width", 2)
+      .attr("fill", "none");
 
 
   }, [data]);
 
   return (
     <div className="session-time">
+      <p>Durée moyenne des sessions</p>
       <svg ref={lineChart}></svg>
+      <div className="infos-lineChart" ref={infosLineChart}></div>
     </div>
   );
 }
