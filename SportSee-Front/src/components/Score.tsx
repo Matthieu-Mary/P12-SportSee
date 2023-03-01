c469b8bbe5c781c14d8c7c5381ce96ed3b382b6b
Matthieu Mary <matthieu.mary2@gmail.com>
2023-03-01 20:02:32 +0100

    add arc to score chart ..

--- SportSee-Front/src/components/Score.tsx
+++ SportSee-Front/src/components/Score.tsx
@@ -3,29 +3,54 @@ import * as d3 from "d3";
 import { USER_MAIN_DATA } from "../mocks/InfosMock";
 
 type Props = {
-  userScore: any
-}
+  userScore: any;
+};
 
-export default function Score({userScore}: Props) {
-
-  const data = USER_MAIN_DATA[0]
+export default function Score({ userScore }: Props) {
+  const data = USER_MAIN_DATA[0];
   const score: number | undefined = data.todayScore;
-  const scoreInPourcent : string = score ? ((score * 100)+"%" ) : "";
-  
+  const scoreInPourcent: number = score ? score * 100 : 0;
+
   const scoreChart: any = useRef(null);
 
   useEffect(() => {
     scoreChart.current.innerHTML = "";
 
-    const currentScoreChart = d3.select(scoreChart.current);
+    const wCurrentScoreChart: number = 258;
+    const hCurrentScoreChart: number = 263;
+    const currentScoreChart = d3
+      .select(scoreChart.current)
+      .attr("width", wCurrentScoreChart)
+      .attr("height", hCurrentScoreChart)
+      .attr(
+        "transform",
+        `translate(${wCurrentScoreChart / 2} ${hCurrentScoreChart / 2})`
+      )
+      .style("overflow", "visible");
 
-    
-  }, [data])
+    // Create Arc
+    const arc = d3
+      .arc<any>()
+      .cornerRadius(10)
+      .innerRadius(80)
+      .outerRadius(90)
+      .startAngle(0)
+      .endAngle((-(scoreInPourcent * 3.6) * (Math.PI / 180)));
+
+    // Extern Red Circle
+    currentScoreChart
+      .append("path")
+      .attr("d", arc)
+      .attr("fill", "#FF0000");
+
+      console.log(Math.PI)
+
+  }, [data]);
 
   return (
     <div className="score-chart">
       <svg ref={scoreChart}></svg>
-      <p>{scoreInPourcent}</p>
+      {/* <p>{scoreInPourcent}</p> */}
     </div>
-  )
+  );
 }
